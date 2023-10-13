b55417135ec84b40245c66c81d0db613319384d3
dicksensei69 <114555396+dicksensei69@users.noreply.github.com>
2023-10-13 10:02:30 -0800

    Update server.js
    this should fix the backend file structure issue, added logic for opening the server to the network with --listen

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+const os = require('os');
 const app = express();
+let localIP;
+
+
+if (process.argv.includes('--listen')) {
+  const networkInterfaces = os.networkInterfaces();
+
+  const interfaces = Object.keys(networkInterfaces)
+    .map(nicName => networkInterfaces[nicName])
+    .reduce((result, nics) => result.concat(nics), []);
+
+  const activeInterface = interfaces.find(nic => nic.family === 'IPv4' && !nic.internal);
+
+  localIP = activeInterface.address;
+
+  app.listen(3000, localIP, () => {
+    console.log('Server started on port 3000', localIP);
+  }); 
+} else {
+  app.listen(3000, () => {
+    console.log('Server started on port 3000', localIP);
+  });
+}
 //this is from the other code to try to auto load the website
 app.get('/', (req, res) => {
   res.sendFile('/home/dick/proj/html-img-viewer-bing/index.html'); 
 });
 //the above is from the other claude code to try to load the website
 app.use(express.json());
-// older one app.use(express.static('public'));
-app.use('/img', express.static(path.join(__dirname, 'public/img')));
+app.use(express.static('public'));
+//app.use('/img', express.static(path.join(__dirname, 'public/img')));
 
 app.get('/list-files', (req, res) => {
   const folder = req.query.folder;
@@ -28,10 +50,12 @@ app.get('/list-files', (req, res) => {
 
 app.put('/update-text', (req, res) => {
   const { filename, text } = req.body;
-  fs.writeFile(filename, text, err => {
-    if (err) return res.status(500).send(err);
-    res.send('Saved');
+  const fullPath = `public/${filename}`;
+  fs.writeFile(fullPath, text, err => {
+    if (err) {
+        console.error(err);
+        return res.status(500).send('Error updating file');}
+    else {
+        res.send('Saved');}
   });
 });
-
-app.listen(3000, () => console.log('Server started on port 3000'));
