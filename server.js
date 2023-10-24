10e7aa6933e22b0227b0ab1f84eec0fd74d7eb36
dicksensei69 <114555396+dicksensei69@users.noreply.github.com>
2023-10-23 23:30:12 -0800

    Update server.js
    added backend connections for tag removal, tag addition to start and tag addition to end, cleaned up a bit and made it so that it can find index.html without a direct link to it so it should work on everyone's machine

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ if (process.argv.includes('--listen')) {
 
   localIP = activeInterface.address;
 
-  app.listen(3000, localIP, () => {
-    console.log('Server started on port 3000', localIP);
+  app.listen(3000, [localIP, 'localhost'], () => {
+    console.log('Server started on localhost & network address:', localIP,':3000');
   }); 
 } else {
   app.listen(3000, () => {
@@ -27,7 +27,8 @@ if (process.argv.includes('--listen')) {
 }
 //this is from the other code to try to auto load the website
 app.get('/', (req, res) => {
-  res.sendFile('/home/dick/proj/html-img-viewer-bing/index.html'); 
+  // res.sendFile('/home/dick/proj/html-img-viewer-bing/excellent-adds-front-back/index.html'); OLDER VERSION
+  res.sendFile(path.join(process.cwd(), 'index.html')); //more universal version that won't break I hope'
 });
 //the above is from the other claude code to try to load the website
 app.use(express.json());
@@ -46,6 +47,121 @@ app.get('/list-files', (req, res) => {
     console.log('Sending response:', { imageFiles, textFiles }); // Log the response
     res.json({ imageFiles, textFiles });
   });
+});
+
+app.post('/remove-words', async (req, res) => {
+
+  // Get words to remove
+   const wordsToRemove = req.body.words;
+  
+  // Get folder path
+  const folder = req.query.folder;
+
+  const textFiles = await fs.promises.readdir(folder);
+
+  for (let file of textFiles) {
+    if (file.endsWith('.txt')) {
+      
+      let filePath = path.join(folder, file);
+
+      let text = await fs.promises.readFile(filePath, 'utf8');
+      
+      // Split text into words
+      const words = text.split(', '); 
+      
+      // Filter out removed words
+      const filtered = words.filter(word => {
+        return !wordsToRemove.includes(word);
+      });
+      
+      // Join words back into string
+      const updatedText = filtered.join(', ');
+      
+      // Save updated text
+      await fs.promises.writeFile(filePath, updatedText);
+    }
+  }
+
+  res.send('Words removed');
+});
+
+// Add start handler
+app.post('/add-start', async (req, res) => {
+
+  // Get words to remove
+   const wordsToAdd = req.body.words;
+  
+  // Get folder path
+  const folder = req.query.folder;
+
+  const textFiles = await fs.promises.readdir(folder);
+
+  for (let file of textFiles) {
+    if (file.endsWith('.txt')) {
+      
+      let filePath = path.join(folder, file);
+
+      let text = await fs.promises.readFile(filePath, 'utf8');
+      
+      // Split text into words
+      const words = text.split(', '); 
+      console.log('Words to add(server.js):', wordsToAdd);
+      // Filter out removed words
+      wordsToAdd.forEach(word => {
+        if (!words.includes(word)) {
+          words.unshift(word); // add to beginning
+        }
+      });
+      
+      // Join words back into string
+      const updatedText = words.join(', ');
+      
+      // Save updated text
+      await fs.promises.writeFile(filePath, updatedText);
+    }
+  }
+
+  res.send('Words added to start');
+});
+
+// Add end handler
+app.post('/add-end', async (req, res) => {
+
+  // Get words to remove
+   const wordsToAdd = req.body.words;
+  
+  // Get folder path
+  const folder = req.query.folder;
+
+  const textFiles = await fs.promises.readdir(folder);
+
+  for (let file of textFiles) {
+    if (file.endsWith('.txt')) {
+      
+      let filePath = path.join(folder, file);
+
+      let text = await fs.promises.readFile(filePath, 'utf8');
+      
+      // Split text into words
+      const words = text.split(', '); 
+      console.log('Words to add(server.js):', wordsToAdd);
+      // Filter out removed words
+      wordsToAdd.forEach(word => {
+        if (!words.includes(word)) {
+          words.push(word); // add to end
+        }
+      });
+      let updatedText = words.join(', ');
+      // Join words back into string
+      updatedText = updatedText.replace(/\r?\n/g, '');
+      //const updatedText = words.join(', ').replace(/\n/g, '');
+      
+      // Save updated text
+      await fs.promises.writeFile(filePath, updatedText);
+    }
+  }
+
+  res.send('Words added to start');  
 });
 
 app.put('/update-text', (req, res) => {
